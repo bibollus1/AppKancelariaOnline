e4916407d5812351c4b35b2323d66a4184fa1251
KBI <bibollus1@gmail.com>
2018-02-15 12:47:45 +0100

    Creating user model

--- app.js
+++ app.js
@@ -9,6 +9,18 @@ require('./config/passport')(passport);
 // Load Routes
 const auth = require('./routes/auth');
 
+// Load Keys
+const keys = require('./config/keys');
+
+// Map global promises
+mongoose.Promise = global.Promise;
+
+// Mongoose connect
+mongoose.connect(keys.mongoURI)
+.then(()=>console.log('MongoDB Connected...'))
+.catch(err=>console.log(err));
+
+// Initialize app
 const app = express();
 
 
--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
-  mongoURI:'mongodb://krzysztofbibik:abcd1234@ds237748.mlab.com:37748/kancelariaonline',
+  mongoURI:'mongodb://krzysztof.bibik:abcd1234@ds237748.mlab.com:37748/kancelariaonline',
+//  mongoURI: 'mongodb://localhost/kancelariaonline',
   googleClientID: '961284861007-qmdi3hmuuav4omrg8arquecggjft81qs.apps.googleusercontent.com',
   googleClientSecret: 'UNPX6hXomm3ZU-hzdRtrSBX1'
 }
