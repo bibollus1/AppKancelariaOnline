8f6058d1523cdb6df63822040ba988e8e984e2a3
KBI <bibollus1@gmail.com>
2018-02-17 09:15:13 +0100

    Working on heroku bug

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',(req, res)=>{
-  res.render('index/Welcome');
+  res.render('index/welcome');
 });
 
 router.get('/dashboard',(req, res)=>{
