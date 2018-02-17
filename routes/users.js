cfa055e2f90058b33f8c5a3877d158da716e8f60
KBI <bibollus1@gmail.com>
2018-02-17 09:07:01 +0100

    Working on heroku bug

--- routes/users.js
+++ routes/users.js
@@ -16,7 +16,7 @@ router.get('/login', (req, res)=>{
 // Login Form POST
 router.post('/login', (req, res, next)=>{
   passport.authenticate('local', {
-    successRedirect: '/ideas',
+    successRedirect: '/',
     failureRedirect: '/users/login',
     failureFlash: true
   })(req, res, next);
